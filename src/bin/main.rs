1d7b80bc0ef1f920c10ee1d67b8c39c2caec1bd9
Atharva <asphadnis1@gmail.com>
2019-06-08 01:56:14 +0530

    Add multithreading functionality

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,14 +2,21 @@ use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
 use std::fs;
+use std::thread;
+use std::time::Duration;
+use webserver::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        //println!("{:?}", stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -18,12 +25,16 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
-    let get_secret = b"GET /secret HTTP/1.1\r\n";
+    let secret = b"GET /secret HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1";
 
     let (status_line, filename) = if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
-    } else if buffer.starts_with(get_secret) {
+    } else if buffer.starts_with(secret) {
         ("HTTP/1.1 200 OK\r\n\r\n", "secret.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
     };
