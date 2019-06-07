c4d7e792387fad77f7a3276cd7f04a6fa743e463
Atharva <asphadnis1@gmail.com>
2019-06-07 16:18:10 +0530

    Add http response and render html

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
+use std::fs;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -17,6 +18,9 @@ fn handle_connection(mut stream: TcpStream) {
 
     stream.read(&mut buffer).unwrap();
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
 
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
